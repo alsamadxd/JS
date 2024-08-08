7959d43169e302c22cf67a161e634cba466091fa
unknown <alsamad0005@gmail.com>
2024-08-08 12:04:27 +0530

    definition

---
+++ 1-Fundamentals/part-1.js
@@ -0,0 +1,24 @@
+//-------------------JAVASCRIPT----------------------------//
+
+/*  High-Level,
+    Prototype Based,
+    Object-oriented,
+    Multi-paradiagm,
+    Interpreted or Just In Time compile,
+    Dynamic,
+    Single-threaded,
+    Garbage-collected Programming Language with First-class function and a non-blocking 
+    Event loop concurrency modal.
+
+*/
+
+//      <script src="fileName.js"></script>
+
+/*  
+    Dynamic typing => We do not have to manually define the datatype of the value to be
+                      stored in a variable, instead the data types are determined 
+                      automatically and we can change the type of value a varible holds.
+
+*/
+
+console.log("hello");
