dca8feb901044331a260b687f6ad205f0a51beae
unknown <alsamad0005@gmail.com>
2024-08-09 16:01:48 +0530

    fundamental part-2

---
+++ 1-Fundamentals/part-2.js
@@ -0,0 +1,33 @@
+//-------------------JAVASCRIPT----------------------------//
+
+/*___________________Paradigm___________________
+
+    An approach and mindset of structuring code, which will direct your coding style
+    and technique.
+
+    Types:
+
+    1- Procedural Programming
+    2- Object Oriented Programmming
+    3- Functional Programming
+*/
+
+/*
+
+    First-class Function => function are treated as varibles, we can pass them into
+                            other funtion, and return them from function.
+    
+    Dynamic => Type of variable can easily be changed.
+*/
+
+/*  
+    Expression => A piece of code that produces a value 
+*/
+5 + 7;
+true;
+/*  
+    Statement => A bigger piece of code does not produces a value on itself.
+*/
+if (15 > 10) {
+  const a = "15 is greater than 10";
+}
