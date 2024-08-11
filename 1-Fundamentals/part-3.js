23b6b3b9995206d075ef00d5f044a54be668d913
unknown <alsamad0005@gmail.com>
2024-08-11 17:03:03 +0530

    Arrays

--- 1-Fundamentals/part-3.js
+++ 1-Fundamentals/part-3.js
@@ -1,1 +1,42 @@
 //-------------------JAVASCRIPT----------------------------//
+
+/*
+
+_______________________FUNCTION___________________________
+
+    An isolated piece of code that can be used again and again.
+    
+    function functionName(){
+        //body
+    }
+*/
+
+// 1- Funtion DECLARATION
+
+function calcAge(birthYear) {
+  return 2024 - birthYear;
+}
+console.log(calcAge(2005));
+
+// 2- Funtion EXPRESSION
+
+const calcAge2 = function (birthYear) {
+  return 2024 - birthYear;
+};
+
+console.log(calcAge2(2005));
+
+// 3- Arrow Function => Special form of function expression
+//                      i.e. shorter and faster to write.
+
+// Ex-1
+
+const calcAge3 = (birthYear) => 2024 - birthYear;
+console.log(calcAge3(2005));
+
+// Ex-2
+
+const calcAge4 = (firstName, birthYear) => {
+  return `${firstName} is ${2024 - birthYear} years old`;
+};
+console.log(calcAge4("Sara", 2005));
